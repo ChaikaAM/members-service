68b0460a7dbb02d98fb38cc9ef9f2f13c9a73765
Aleksei_Chaika <g_thing@mail.ru>
2018-07-26 16:18:57 +0300

    A little bit of logging

--- src/main/java/com/adviqo/members/service/MemberService.java
+++ src/main/java/com/adviqo/members/service/MemberService.java
@@ -5,6 +5,7 @@ import com.adviqo.members.exception.IllegalMemberDtoException;
 import com.adviqo.members.exception.MemberNotFoundException;
 import com.adviqo.members.model.Member;
 import com.adviqo.members.repository.MemberRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @Service
+@Log4j2
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -23,7 +25,9 @@ public class MemberService {
     }
 
     public final Member save(@Valid MemberDto memberDto) {
+        log.info("Saving member {}", memberDto);
         if (memberDto.getFirstName() == null || memberDto.getFirstName().isEmpty()) {
+            log.error("Member could not be saved with null or empty firstName, bad data - {}", memberDto);
             throw new IllegalMemberDtoException("Member should have at least first name parameter");
         }
         return memberRepository.save(memberDto.toMember());
@@ -39,8 +43,10 @@ public class MemberService {
 
     public final Member updateById(Long id, MemberDto memberDto) {
         final Member existingMember = findById(id);
+        log.info("Updating member {} by data {}", existingMember, memberDto);
         if (memberDto.getFirstName() != null) {
             if (memberDto.getFirstName().isEmpty()) {
+                log.error("Member could not be updated with empty firstName, bad data - {}", memberDto);
                 throw new IllegalMemberDtoException("Member could not be updated with empty first name parameter");
             }
             existingMember.setFirstName(memberDto.getFirstName());
@@ -60,7 +66,9 @@ public class MemberService {
     public final void deleteMember(Long id) {
         try {
             memberRepository.deleteById(id);
+            log.info("Member with id {} has been deleted", id);
         } catch (EmptyResultDataAccessException e) {
+            log.error("Could not delete member with id {}. Not found", id);
             throw new MemberNotFoundException("Member could not be deleted because id has not been found in database");
         }
     }
